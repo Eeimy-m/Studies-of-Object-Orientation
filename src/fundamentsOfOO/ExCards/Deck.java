35b6fbfec5b35ee36d9d1ccf632db3e66d25b563
lucas-ifsp <lucas.ruas@gmail.com>
2026-03-18 10:01:09 -0300

    feat: add review comments as TODOs

--- src/fundamentsOfOO/ExCards/Deck.java
+++ src/fundamentsOfOO/ExCards/Deck.java
@@ -5,4 +5,8 @@ public class Deck {
     public Deck() {
 
     }
+
+    // TODO criar um array de cartas para gerar as 52 combinações possíveis
+    // TODO criar um método para retirar uma carta do baralho e retornar para quem pediu
+    // TODO criar um método para retirar n cartas do baralho, retornando-as para quem pediu.
 }
