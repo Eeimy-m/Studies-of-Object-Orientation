7d5838620ab1c00bfdbf31bbe7f2665cb9c1da61
Elisa <eeimymiura@gmail.com>
2026-03-24 08:44:58 -0300

    Relacionamentos, contadores e construtor da classe UserAccoount

--- src/sc3050742_P1/UserAccount.java
+++ src/sc3050742_P1/UserAccount.java
@@ -4,6 +4,21 @@ public class UserAccount {
     private String email;
     private String userName;
 
+    //Relacionamentos
+    private UserAccount[] followers;
+    private Post[] myPosts;
+    private Post[] timeline;
+
+    //Contadores de controle dos relacionamentos
+    private int numOfFollowers;
+    private int numOfMyPosts;
+    private int numOfTimelinePosts;
+
+    public UserAccount(String email, String userName) {
+        this.email = email;
+        this.userName = userName;
+    }
+
     public String getUserName() {
         return userName;
     }
