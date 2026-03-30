f36da20160330bf22acc97ffa4fe44c9ddc0cde2
Elisa <eeimymiura@gmail.com>
2026-03-30 10:24:32 -0300

    addMeeting fixed

---
+++ src/LocalDateTime/Main.java
@@ -0,0 +1,29 @@
+package LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        Meeting meeting1 = new Meeting(
+                "Reunião 1 lab 105",
+                LocalTime.of(10, 0),
+                LocalTime.of(11,0)
+        );
+
+        Meeting meeting2 = new Meeting(
+                "Reunião 2 lab 200",
+                LocalTime.of(10, 30),
+                LocalTime.of(11,30)
+        );
+
+        Schedule schedule1 = new Schedule(
+                LocalDate.of(2026, 3, 30),
+                LocalTime.of(8, 0),
+                LocalTime.of(16, 0)
+        );
+
+        schedule1.addMeeting(meeting1);
+        schedule1.addMeeting(meeting2);
+//        schedule1.removeMeeting(meeting1);
+    }
+}
--- src/LocalDateTime/Schedule.java
+++ src/LocalDateTime/Schedule.java
@@ -1,15 +1,13 @@
 package LocalDateTime;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class Schedule {
     private LocalDate day;
     private LocalTime startTime;
     private LocalTime endTime;
 
-    private Meeting[] meetings;
+    private Meeting[] meetings = new Meeting[2];
     private int cont;
 
     public Schedule(LocalDate day, LocalTime startTime, LocalTime endTime) {
@@ -20,14 +18,14 @@ public class Schedule {
 
     public void addMeeting(Meeting meeting) {
         if(cont != 0) {
-            for(Meeting meetingScheduled : meetings) {
-                LocalTime meetingStartTime = meetingScheduled.getStartTime();
-                LocalTime meetingEndTime = meetingScheduled.getEndTime();
+            for(int i = 0; i < cont; i++) {
+                Meeting meetingScheduled = meetings[i];
 
-                if(meetingStartTime.isAfter(meeting.getStartTime()) && meetingStartTime.isBefore(meeting.getEndTime()))
-                    break;
-                else if(meetingEndTime.isAfter(meeting.getStartTime()) && meetingEndTime.isBefore(meeting.getEndTime()))
-                    break;
+                boolean conflict = meeting.getStartTime().isBefore(meetingScheduled.getEndTime()) &&
+                        meeting.getEndTime().isAfter(meetingScheduled.getStartTime());
+
+                if(conflict)
+                    return;
             }
         }
 
@@ -36,7 +34,18 @@ public class Schedule {
     }
 
     public void removeMeeting(Meeting meeting) {
-
+        int counter = 0;
+        if(cont != 0) {
+            for(Meeting meetingOfTheday : meetings) {
+                if(meetingOfTheday != meeting) counter++;
+                else {
+                    meetings[counter] = null;
+                    cont--;
+                    System.out.println("Meeting removed:" + meeting.getDescription());
+                    break;
+                }
+            }
+        }
     }
 
 //    public double percentageSpentInMeetings() {
