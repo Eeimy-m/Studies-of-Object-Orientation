bd9db4649a44e455f8211233756ab72a2556c21a
Elisa <eeimymiura@gmail.com>
2026-03-30 09:23:40 -0300

    method addMeeting done

--- src/LocalDateTime/Schedule.java
+++ src/LocalDateTime/Schedule.java
@@ -9,27 +9,43 @@ public class Schedule {
     private LocalTime startTime;
     private LocalTime endTime;
 
+    private Meeting[] meetings;
+    private int cont;
+
     public Schedule(LocalDate day, LocalTime startTime, LocalTime endTime) {
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
-    public void addMeeting() {
+    public void addMeeting(Meeting meeting) {
+        if(cont != 0) {
+            for(Meeting meetingScheduled : meetings) {
+                LocalTime meetingStartTime = meetingScheduled.getStartTime();
+                LocalTime meetingEndTime = meetingScheduled.getEndTime();
+
+                if(meetingStartTime.isAfter(meeting.getStartTime()) && meetingStartTime.isBefore(meeting.getEndTime()))
+                    break;
+                else if(meetingEndTime.isAfter(meeting.getStartTime()) && meetingEndTime.isBefore(meeting.getEndTime()))
+                    break;
+            }
+        }
+
+        meetings[cont] = meeting;
+        cont++;
+    }
+
+    public void removeMeeting(Meeting meeting) {
 
     }
 
-    public void removeMeeting() {
-
-    }
-
-    public double percentageSpentInMeetings() {
-
-    }
-
-    public String  scheduleAsString() {
-
-    }
+//    public double percentageSpentInMeetings() {
+//
+//    }
+//
+//    public String  scheduleAsString() {
+//
+//    }
 
     public LocalDate getDay() {
         return day;
