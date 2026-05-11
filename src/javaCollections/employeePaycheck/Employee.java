cb8911a3f203ed562faf7fdb7a275a80c57929d2
Elisa <eeimymiura@gmail.com>
2026-05-11 10:55:41 -0300

    Employee class methods

--- src/javaCollections/employeePaycheck/Employee.java
+++ src/javaCollections/employeePaycheck/Employee.java
@@ -1,6 +1,9 @@
 package javaCollections.employeePaycheck;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.Iterator;
+import java.util.List;
 
 public class Employee {
     private String id;
@@ -9,12 +12,30 @@ public class Employee {
     private double salary;
     private LocalDate dateOfEmployment;
 
+    private List<Paycheck> paychecks;
+
     public Employee(String id, String name, String jobTitle, double salary, LocalDate dateOfEmployment) {
         this.id = id;
         this.name = name;
         this.jobTitle = jobTitle;
         this.salary = salary;
         this.dateOfEmployment = dateOfEmployment;
+    }
+
+    public double getYearsOfService() {
+        return (double) (Period.between(dateOfEmployment, LocalDate.now()).toTotalMonths()/12);
+    }
+
+    public void addPaycheck(LocalDate payDay) {
+        paychecks.add(new Paycheck(payDay, salary));
+    }
+
+    public void removePaycheck(Paycheck paycheck) {
+        paychecks.remove(paycheck);
+    }
+
+    public Iterator<Paycheck> iteratorPaycheck() {
+        return paychecks.iterator();
     }
 
     public String getId() {
