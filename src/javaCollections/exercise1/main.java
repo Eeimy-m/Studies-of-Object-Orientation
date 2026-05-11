678988250f3e400af6f06cc5755827e98ae33aad
Elisa <eeimymiura@gmail.com>
2026-05-11 09:17:59 -0300

    treeSet and ArrayList

---
+++ src/javaCollections/exercise1/main.java
@@ -0,0 +1,29 @@
+package javaCollections.exercise1;
+
+import java.util.*;
+
+public class main {
+    static void main() {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < 4; i++) {
+            stringArrayList.add(scanner.next());
+        }
+
+        StringJoiner stringJoiner = new StringJoiner(",");
+        for(String string : stringArrayList) {
+            stringJoiner.add(string);
+        }
+        System.out.println(stringJoiner);
+
+        Set<String> treeSet = new TreeSet<>(stringArrayList);
+
+        System.out.println("TreeSet: ");
+        for(String string : treeSet) {
+            System.out.println(string);
+        }
+
+        System.out.println("Frequência de cada palavra digitada: ");
+    }
+}
